5354d2de62f6caed395c52c8f14945adbad08660
Petrushchenko <petrushchenko.l@ukr.net>
2018-10-18 18:36:42 +0300

    good working function gameCompleted

--- script.js
+++ script.js
@@ -4,16 +4,13 @@
   let field = document.querySelector('.square');
   const suffleBtn = document.querySelector(".btn");
   const emptyCell = document.querySelector(".cell.empty");
-  const cells = document.querySelectorAll('.cell');
-
 
   var initGame = function () {
-    suffleBtn.addEventListener("click",  shuffleTiles);
+    suffleBtn.addEventListener("click", () => shuffleTiles(field));
     window.addEventListener('keydown', moveCell);
-
   }
 
-  var random = function (parent){
+  var shuffleTiles = function (parent){
     let elementsArr = parent.querySelectorAll('div');
     for(var i = 0; i < 16; i++) {
       let el1 = Math.floor(Math.random() * 16 );
@@ -24,25 +21,20 @@
     }
   }
 
-  var shuffleTiles = function (e){
-    random(field);
-  }
-
   var gameCompleted = function (nodeList){
     let ar = [...nodeList];
     let victory = ar.filter((cell, i) => i < 14).every((cell,j) => j + 1 == cell.textContent);
-
     if (victory) {alert('victory');}
   }
+
   var moveLeft = function(){
     let firstColumn = field.querySelectorAll('div:nth-child(4n+1)');
-
     let isEndOfRow = Array.prototype.some.call(firstColumn, (cell) => emptyCell.nextElementSibling === null || cell.textContent === emptyCell.nextElementSibling.textContent);
   
     if (isEndOfRow) {return;}
     field.insertBefore(emptyCell.nextElementSibling, emptyCell);
-
   }
+
   var moveRight =function() {
     let lastColumn = field.querySelectorAll('div:nth-child(4n)');
 
@@ -54,23 +46,26 @@
   }
 
   var moveUp = function(){
+    var cells = field.querySelectorAll('.cell');
+
     let isLastRow = Array.prototype.indexOf.call(cells, emptyCell);
+    
     if (isLastRow > 11) {return;}
      else {
       let cellToMove = Array.prototype.filter.call(cells, (cell, i) => i == isLastRow + 4)[0];
       field.insertBefore(cellToMove, emptyCell);
       field.insertBefore(emptyCell, cells[isLastRow + 5]);
      }
- 
   }
 
   var moveDown =function() {
+    var cells = document.querySelectorAll('.cell');
     let isFirstRow = Array.prototype.indexOf.call(cells, emptyCell);
+    
     if (isFirstRow < 4) {return;}
      else {
       let cellToMove = Array.prototype.filter.call(cells, (cell, i) => i == isFirstRow - 4)[0];
       field.insertBefore(cellToMove, emptyCell);
-
       field.insertBefore(emptyCell, cells[isFirstRow -3]);
      }
 
@@ -80,7 +75,8 @@
     const ARROW_UP = 38;
     const ARROW_RIGHT = 39;
     const ARROW_DOWN = 40;
-    
+    var cells = document.querySelectorAll('.cell');
+       
     if (
       e.keyCode !== ARROW_LEFT 
       && e.keyCode !== ARROW_UP 
@@ -108,11 +104,9 @@
       break;  
     }
   gameCompleted(cells);
-
   }
 
-  initGame();
-   
+  initGame(); 
 })();
 
 
